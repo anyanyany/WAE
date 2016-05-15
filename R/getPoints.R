21143cf89aaaf656d0e1474c0ab09a293cb5830f
Anna Zawadzka <Anna Zawadzka>
2016-05-15 18:42:19 +0200

    new GetPointsHyperMesh function

--- R/getPoints.R
+++ R/getPoints.R
@@ -11,9 +11,17 @@ GetPointsUniformDistribution = function(dimension,N,a,b)
 GetPointsHyperMesh = function(dimension,N,a,b)
 {
   points=list();
-  for(i in c(1:N))
+  scale=ceiling(N^(1/dimension)); #podzia³ka siatki - sufit z pierwiastka z N stopnia dimension
+  h=(b-a)/(scale-1);
+  values=array(dim=scale);
+  for(i in c(1:scale))
   {
-    points[[i]]=runif(dimension, a, b);
+    values[i]=a+(i-1)*h;
+  }
+  allPoints=permutations(scale, dimension, v=values, set=TRUE, repeats.allowed=TRUE)
+  for(i in c(1:N)) #obcina sie nadwyzka punktow
+  {
+    points[[i]]=allPoints[i,];
   }
   return (points);
 }
