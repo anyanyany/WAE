8958eb045c5501f1de680079cb23583d5134bedf
Piotr Waszkiewicz <waszka23@gmail.com>
2016-05-20 20:13:13 +0200

    Reverted changes in main

--- R/main.R
+++ R/main.R
@@ -7,8 +7,8 @@ source('./randomNeighbor.R')
 
 main=function()
 {
-  dimensions=c(2,5);
-  functions=c(1:2);
+  dimensions=c(2,5,10,20,30,40,50,60,70,80,90,100);
+  functions=c(1:28);
   N=10; #N-ile punktow
   delta=1e-2;
   a=-1;
